6b0aa4334f52eb2c6b83d0e1d30be629ac36364e
abtin.amraei <abtin.amraei@gmail.com>
2025-08-08 00:53:11 +0330

    hyby

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductListByCategory
 
 urlpatterns = [
-    path('products/', ProductListByCategory.as_view(), name='product-list-by-category'),
+    path('products/', ProductListByCategory.as_view(), name='products-list'),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # همه api ها زیر /api/
+    path('api/', include('api.urls')),  # مسیر api اضافه شده باشد
 ]
