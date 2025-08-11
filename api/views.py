06503139db3af23cb2f1189f0510339a6e357760
abtin.amraei <abtin.amraei@gmail.com>
2025-08-11 22:04:28 +0330

    czxcfvbnj

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from django.db.models import Q
+from rest_framework.generics import RetrieveAPIView  # اضافه شده برای جزئیات محصول
 
 from .models import EmailVerificationCode, Category, Product
 from .serializers import (
@@ -108,6 +109,12 @@ class ProductListByCategory(generics.ListAPIView):
             )
 
         return queryset
+
+
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class UserProfileView(APIView):
