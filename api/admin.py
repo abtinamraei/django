7cea3c3ca2e8a9e667e704ea90e071cde646ddaa
abtin.amraei <abtin.amraei@gmail.com>
2025-08-12 18:02:48 +0330

    wlskkd

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, ProductVariant
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1  # تعداد فرم‌های خالی برای اضافه کردن وریانت جدید
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+    inlines = [ProductVariantInline]
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductVariant)
