9b2f3c1ba19d99ea8a48de3215942c07f488f242
abtin.amraei <abtin.amraei@gmail.com>
2025-08-12 22:23:58 +0330

    Sdmlsdnv

--- api/admin.py
+++ api/admin.py
@@ -1,23 +1,18 @@
 from django.contrib import admin
 from .models import Category, Product, ProductColor, ProductSize
 
+class ProductColorInline(admin.TabularInline):
+    model = ProductColor
+    extra = 1
+
 class ProductSizeInline(admin.TabularInline):
     model = ProductSize
     extra = 1
 
-class ProductColorInline(admin.TabularInline):
-    model = ProductColor
-    extra = 1
-    inlines = [ProductSizeInline]  # اینجا نمی‌شود مستقیم اینلاین توی اینلاین گذاشت، باید روش جایگزین داشته باشی
-
-class ProductColorAdmin(admin.ModelAdmin):
-    inlines = [ProductSizeInline]
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'price']
-    inlines = [ProductColorInline]
+    inlines = [ProductColorInline, ProductSizeInline]
 
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductColor, ProductColorAdmin)
-# admin.site.register(ProductSize)  # چون در رنگ به صورت inline هست، نیازی به ثبت جداگانه نیست
+# نیازی نیست رنگ و سایز رو جدا ثبت کنیم چون داخل inline هستن
