66345bfe5f311dc6d551d38500fcd87cbf14cab5
abtin.amraei <abtin.amraei@gmail.com>
2025-08-12 22:37:02 +0330

    ksrgilsg

--- api/models.py
+++ api/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+
+class EmailVerificationCode(models.Model):
+    email = models.EmailField(unique=True)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        # کد بعد از 10 دقیقه منقضی می‌شود
+        expiration_time = self.created_at + timedelta(minutes=10)
+        return timezone.now() > expiration_time
+
+    def __str__(self):
+        return f"{self.email} - {self.code}"
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # پایه یا مینیموم قیمت میشه از سایز گرفت
 
     def __str__(self):
         return self.name
@@ -21,17 +36,16 @@ class Product(models.Model):
 
 class ProductColor(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='colors')
-    color = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    hex_code = models.CharField(max_length=7, blank=True, null=True, help_text="مثلا #FF5733")
 
     def __str__(self):
-        return f'{self.product.name} - {self.color}'
+        return f"{self.product.name} - {self.name}"
 
 
 class ProductSize(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='sizes')
-    size = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField(default=0)
+    size = models.CharField(max_length=20)  # مثلا S, M, L, XL یا عددی
 
     def __str__(self):
-        return f'{self.product.name} - {self.size} - {self.price}'
+        return f"{self.product.name} - {self.size}"
